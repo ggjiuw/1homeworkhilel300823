48c170916fbdc9cb9f0c8021d20bfb0e12a60e6a
ggjiuw <herezoom1231@gmail.com>
2023-09-09 12:00:42 +0300

    replaced default_result on raise ValueError

--- car_params.py
+++ car_params.py
@@ -1,8 +1,7 @@
 
 def car_info(weight, time, speed: int | float) -> str:
-    default_result = 'ValueError'
     if speed <= 0 or time <= 0 or weight <= 0:
-        return default_result
+        raise ValueError('The entered auto parameters are incorrect, they cannot be equal to or less than zero')
         exit()
     result = f'Транспортний засіб вагою {weight} кг рухався {time} секунд зі швидкістю {speed}м/сек, і подолав відстань {time * speed} метрів'
     return result
